2e52f315a03e591869a086a18962e15b61ebff8f
CookedMeat2 <73088824+CookedMeat2@users.noreply.github.com>
2020-10-20 14:03:22 +0000

    Update v2ray_ws_tls1.3.sh

--- v2ray_ws_tls1.3.sh
+++ v2ray_ws_tls1.3.sh
@@ -13,8 +13,8 @@ function yellow(){
 }
 
 function check_os(){
-green "系统支持检测"
-sleep 6
+green "系统支持检测……"
+sleep 5
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
     systemPackage="yum"
@@ -66,40 +66,60 @@ elif [ "$release" == "ubuntu" ]; then
     red "==============="
     exit
     fi
-    green "支持的系统"
-    ufw_status=`systemctl status ufw | grep "Active: active"` &>/dev/null 2>&1
-    if [ -n "$ufw_status" ]; then
-        ufw allow 80/tcp >/dev/null 2>&1
-        ufw allow 443/tcp >/dev/null 2>&1
-    fi
-    apt-get update >/dev/null 2>&1
-    green "安装nginx编译依赖"
-    apt-get install -y vim vnstat htop build-essential libpcre3 libpcre3-dev zlib1g-dev liblua5.1-dev libluajit-5.1-dev libgeoip-dev google-perftools libgoogle-perftools-dev >/dev/null 2>&1
+    clear
+    green "=============================================="
+    green "系统支持检测：支持当前系统"
+    green "=============================================="
+    green
+    green "安装nginx编译需要的软件"
+    $systemPackage update >/dev/null 2>&1
+    $systemPackage install -y vim vnstat htop wget curl unzip build-essential libpcre3 libpcre3-dev zlib1g-dev liblua5.1-dev libluajit-5.1-dev libgeoip-dev google-perftools libgoogle-perftools-dev >/dev/null 2>&1
 elif [ "$release" == "debian" ]; then
-    apt-get update >/dev/null 2>&1
-    green "安装nginx编译依赖"
-    apt-get install -y vim vnstat htop build-essential libpcre3 libpcre3-dev zlib1g-dev liblua5.1-dev libluajit-5.1-dev libgeoip-dev google-perftools libgoogle-perftools-dev >/dev/null 2>&1
+    $systemPackage update >/dev/null 2>&1
+    green "安装nginx编译需要的软件"
+    $systemPackage install -y vim vnstat htop wget curl unzip build-essential libpcre3 libpcre3-dev zlib1g-dev liblua5.1-dev libluajit-5.1-dev libgeoip-dev google-perftools libgoogle-perftools-dev >/dev/null 2>&1
 fi
 }
 
 function check_env(){
-green "安装环境监测"
-sleep 6
+green
+green "检测nginx安装环境"
+sleep 5
 if [ -f "/etc/selinux/config" ]; then
     CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
     if [ "$CHECK" != "SELINUX=disabled" ]; then
+        green "=============================================="
         green "检测到SELinux开启状态，添加开放80/443端口规则"
-	yum install -y policycoreutils-python >/dev/null 2>&1
+        green "=============================================="
+	      yum install -y policycoreutils-python >/dev/null 2>&1
         semanage port -m -t http_port_t -p tcp 80
         semanage port -m -t http_port_t -p tcp 443
     fi
 fi
-firewall_status=`firewall-cmd --state` &>/dev/null 2>&1
-if [ "$firewall_status" == "running" ]; then
-    green "检测到firewalld开启状态，添加放行80/443端口规则"
+
+systemctl status ufw &>/dev/null
+if [ "$?" -eq "0" ]; then
+  ufw_status=`systemctl status ufw | grep "Active: active"`
+  if [ -n "$ufw_status" ]; then
+      ufw allow 80/tcp >/dev/null 2>&1
+      ufw allow 443/tcp >/dev/null 2>&1
+      green
+      green "=============================================="
+      green "检测到ufw防火墙开启，添加放行80/443端口规则"
+      green "=============================================="
+      green
+  fi
+fi
+
+firewall-cmd --state &>/dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  firewall_status=`firewall-cmd --state`
+  if [ "$firewall_status" == "running" ]; then
+    green "检测到firewalld开启，添加放行80/443端口规则"
     firewall-cmd --zone=public --add-port=80/tcp --permanent >/dev/null 2>&1
     firewall-cmd --zone=public --add-port=443/tcp --permanent >/dev/null 2>&1
     firewall-cmd --reload >/dev/null 2>&1
+  fi
 fi
 $systemPackage -y install net-tools socat >/dev/null 2>&1
 Port80=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 80`
@@ -107,23 +127,20 @@ Port443=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 443`
 if [ -n "$Port80" ]; then
     process80=`netstat -tlpn | awk -F '[: ]+' '$5=="80"{print $9}'`
     red "==================================================================="
-    red "检测到80端口被占用，占用进程为：${process80}，请先结束相关服务和进程"
+    red "检测到80端口被占用，占用进程：${process80}，请先结束相关服务和进程"
     red "==================================================================="
     exit 1
 fi
 if [ -n "$Port443" ]; then
     process443=`netstat -tlpn | awk -F '[: ]+' '$5=="443"{print $9}'`
     red "====================================================================="
-    red "检测到443端口被占用，占用进程为：${process443}，请先结束相关服务和进程"
+    red "检测到443端口被占用，占用进程：${process443}，请先结束相关服务和进程"
     red "====================================================================="
     exit 1
 fi
 }
 
-
-#安装nginx
 function install(){
-    $systemPackage install -y wget curl unzip >/dev/null 2>&1
     green "=============================================="
     yellow "请输入要绑定到本VPS的域名，可以是二级域名"
     green "=============================================="
@@ -132,12 +149,12 @@ function install(){
     real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
     local_addr=`curl ipv4.icanhazip.com`
     if [ $real_addr == $local_addr ] ; then
-    green "=========================================="
+  green "=========================================="
 	green "       域名解析正常，开始安装nginx"
 	green "=========================================="
         install_nginx
     else
-    red "===================================="
+  red "===================================="
 	red "域名解析地址与本VPS的IP地址不一致"
 	red "若确认解析成功,可强制脚本继续运行"
 	red "===================================="
@@ -164,11 +181,16 @@ function install_nginx(){
     tar xf nginx-1.15.8.tar.gz && rm nginx-1.15.8.tar.gz >/dev/null 2>&1
     cd nginx-1.15.8
     ./configure --prefix=/etc/nginx --with-openssl=../openssl-1.1.1a --with-openssl-opt='enable-tls1_3' --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_stub_status_module --with-http_sub_module --with-stream --with-stream_ssl_module  >/dev/null 2>&1
-    green "开始编译安装nginx及组件，可能等待时间较长，"
+    green
+    green "=========================================="
+    green "正在编译安装nginx及组件，可能等待时间较长，"
     green "通常需要5到10分钟，可以去喝口水或小憩一下？"
-    sleep 6
-    make >/dev/null 2>&1
-    make install >/dev/null 2>&1
+    green "=========================================="
+    green
+    sleep 5
+    make
+    make install
+    clear
 
 cat > /etc/nginx/conf/nginx.conf <<-EOF
 user  root;
@@ -192,7 +214,7 @@ http {
     #gzip  on;
     include /etc/nginx/conf.d/*.conf;
 
-# 不使用v2ray的TLS，用Nginx实现TLS
+# 不使用v2ray自带的TLS，使用Nginx的TLS
 server {
     listen  443 ssl;
     server_name           $your_domain;
@@ -227,10 +249,21 @@ server {
     }
 }
 
-# http跳到https
+# 同一个VPS另外一个网站的域名，通过nginx跳转到443端口
+server {
+  listen       443 ssl;
+  server_name      xxxxxxx.com;  #修改为另外一个域名
+  ssl_certificate       /etc/nginx/ssl/fullchain.cer;
+  ssl_certificate_key   /etc/nginx/ssl/$your_domain.key;
+  location / {
+  proxy_pass https://$real_addr:44399;  #将其他网站的443端口改为44399
+  }
+}
+
+# 将解析到此IP的域名、http地址，重定向到对应的https网址上
 server {
     listen          80;
-    server_name      $your_domain www.$short_domain;
+    server_name      $your_domain *.$short_domain xxxxxxx.com;   #修改为另外一个域名
     return 301 https://\$server_name\$request_uri;
     }
 }
@@ -357,7 +390,7 @@ rm -f /usr/local/etc/v2ray/qr_config.json
 
 cat > /usr/local/etc/v2ray/myconfig.json<<-EOF
 {
-=========手动设置参数===========
+=============账 号 信 息===============
 地址：${real_addr}
 端口：443
 uuid：${v2uuid}
@@ -368,18 +401,20 @@ uuid：${v2uuid}
 路径：${newpath}
 底层传输：tls
 
+Qv2ray二维码链接：${v2ray_link}
+v2rayN二维码链接：${v2ray_link}
+
 nginx配置：/etc/nginx/conf/nginx.conf
 v2ray配置：/usr/local/etc/v2ray/config.json
-
-Qv2ray二维码链接：${v2ray_link}
 
 }
 EOF
 
 green "==============================="
-green "         安装已经完成"
-green "=========手动设置参数=========="
-green "地址：${your_domain}"
+green "  安装已经完成，账号信息如下："
+green "==============================="
+green
+green "地址：${real_addr}"
 green "端口：443"
 green "uuid：${v2uuid}"
 green "额外id：64"
@@ -389,106 +424,88 @@ green "别名：Vmess_${newpath}"
 green "路径：${newpath}"
 green "底层传输：tls"
 green
-green "nginx配置：/etc/nginx/conf/nginx.conf"
-green "v2ray配置：/usr/local/etc/v2ray/config.json"
-green "当前设置信息 ：/usr/local/etc/v2ray/myconfig.json"
-green
 green "Qv2ray二维码链接：${v2ray_link}"
+green "V2rayN二维码链接：${v2ray_link}"
+green
+green "Nginx配置：/etc/nginx/conf/nginx.conf"
+green "V2ray配置：/usr/local/etc/v2ray/config.json"
 green
 }
 
-
-function web_download() {
-web_dir="/etc/nginx/html"
-  [[ ! -d "${web_dir}" ]] && mkdir "${web_dir}"
-  while 1 = 1; do
-    green "请选择下面任意一个网站模板:"
-    green "1. https://templated.co/intensify(素雅模板)"
-    green "2. https://templated.co/binary(人物照片)"
-    green "3. https://templated.co/retrospect(风景照片)"
-    green "4. https://templated.co/spatial(山林照片)"
-    green "5. https://templated.co/monochromed(灰色城市)"
-    green "6. https://templated.co/transit(博客留言)"
-    green "7. https://templated.co/interphase(靓丽城市)"
-    green "8. https://templated.co/ion(大山照片)"
-    green "9. https://templated.co/solarize(绿荫照片)"
-    green "10. https://templated.co/phaseshift(绿荫照片)"
-    green "11. https://templated.co/horizons(红果照片)"
-    green "12. https://templated.co/grassygrass(绿草照片)"
-    green "13. https://templated.co/breadth(指南针照片)"
-    green "14. https://templated.co/undeviating(高楼蓝天)"
-    green "15. https://templated.co/lorikeet(绿色鹦鹉)"
-    read -p "请输入要下载的网站数字:" site
-    case $site in
+function web_download () {
+  rm -rf /etc/nginx/html
+  mkdir /etc/nginx/html
+  cd /etc/nginx/html
+  green "请选择下面任意一个伪装网站模板，安装之前可以查看网站demo:"
+  green "1. https://templated.co/intensify(素雅模板)"
+  green "2. https://templated.co/binary(人物照片)"
+  green "3. https://templated.co/retrospect(风景照片)"
+  green "4. https://templated.co/spatial(山林照片)"
+  green "5. https://templated.co/monochromed(灰色城市)"
+  green "6. https://templated.co/transit(博客留言)"
+  green "7. https://templated.co/interphase(靓丽城市)"
+  green "8. https://templated.co/ion(大山照片)"
+  green "9. https://templated.co/solarize(绿荫照片)"
+  green "10. https://templated.co/phaseshift(绿荫照片)"
+  green "11. https://templated.co/horizons(红果照片)"
+  green "12. https://templated.co/grassygrass(绿草照片)"
+  green "13. https://templated.co/breadth(指南针照片)"
+  green "14. https://templated.co/undeviating(高楼蓝天)"
+  green "15. https://templated.co/lorikeet(绿色鹦鹉)"
+  read -p "请输入要下载的网站数字编号:" siteNum
+  case $siteNum in
     1)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/intensify/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/intensify/download >/dev/null 2>&1
     ;;
     2)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/binary/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/binary/download >/dev/null 2>&1
     ;;
     3)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/retrospect/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/retrospect/download >/dev/null 2>&1
     ;;
     4)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/spatial/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/spatial/download >/dev/null 2>&1
     ;;
     5)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/monochromed/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/monochromed/download >/dev/null 2>&1
     ;;
     6)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/transit/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/transit/download >/dev/null 2>&1
     ;;
     7)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/interphase/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/interphase/download >/dev/null 2>&1
     ;;
     8)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/ion/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/ion/download >/dev/null 2>&1
     ;;
     9)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/solarize/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/solarize/download >/dev/null 2>&1
     ;;
     10)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/phaseshift/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/phaseshift/download >/dev/null 2>&1
       ;;
     11)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/horizons/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/horizons/download >/dev/null 2>&1
     ;;
     12)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/grassygrass/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/grassygrass/download >/dev/null 2>&1
     ;;
     13)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/breadth/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/breadth/download >/dev/null 2>&1
     ;;
     14)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/undeviating/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/undeviating/download >/dev/null 2>&1
     ;;
     15)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/lorikeet/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/lorikeet/download >/dev/null 2>&1
     ;;
     *)
-      rm -f ./*
-      wget -O ${web_dir}/web.zip --no-check-certificate https://templated.co/intensify/download >/dev/null 2>&1
+      wget -O web.zip --no-check-certificate https://templated.co/intensify/download >/dev/null 2>&1
     ;;
     esac
-  done
-  unzip -o -d ${web_dir} ${web_dir}/web.zip
-    green "网站已切换，请在浏览器查看"
-    sleep 6
+    unzip -o -d /etc/nginx/html/web.zip
+    green "网站已切换，请在浏览器查看。"
+    sleep 5
 }
 
 
@@ -496,8 +513,8 @@ function change_bbr() {
   wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
   chmod +x tcp.sh
   ./tcp.sh
-    green "操作完成！"
-    sleep 6
+  green "5秒后返回"
+  sleep 5
 }
 
 function install_bbr() {
@@ -516,16 +533,16 @@ function install_bbr() {
 	fi
 	if [[ $is_bbr ]]; then
 		echo
-		green "BBR 已启用，无需再安装"
+		green "BBR 加速已启用，无需再安装"
 		echo
-	elif [[ $try_enable_bbr ]]; then
+    elif [[ $try_enable_bbr ]]; then
 		sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 		sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 		echo "net.ipv4.tcp_congestion_control = bbr" >>/etc/sysctl.conf
 		echo "net.core.default_qdisc = fq" >>/etc/sysctl.conf
 		sysctl -p >/dev/null 2>&1
 		echo
-		green "已为你的 VPS 启用 BBR 加速"
+		green "BBR 加速已成功启用"
 		echo
 	else
 		# https://teddysun.com/489.html
@@ -536,10 +553,9 @@ function install_bbr() {
 function update_v2ray() {
     bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
     systemctl restart v2ray
-    green "v2ray更新完成"
-    sleep 6
+    green "V2ray已更新"
+    sleep 5
 }
-
 
 function remove_v2ray_nginx() {
 
@@ -553,28 +569,33 @@ function remove_v2ray_nginx() {
     rm -rf /etc/systemd/system/v2ray*
     rm -rf /etc/nginx
 
-    green "卸载完成，系统已还原！"
-    sleep 6
+    green "卸载完成，系统已还原"
+    sleep 5
 
 }
 
 function start_menu(){
     clear
     green " ========================================================="
-    green " 介绍: 一键安装 V2ray+WS+TLS+CDN，支持CF自选节点"
+    green " 介绍: 一键安装 V2ray+ws+tls+CDN，支持cf自选节点。"
     green " 支持: Centos7/Debian9+/Ubuntu16.04+"
     green " 时间: 2020-10-19"
+    green " 作者: CookedMeat2"
     green " ========================================================="
     echo
-    green " 1. 安装 V2ray+WS+TLS"
-    green " 2. 更新 V2ray主程序"
-    green " 3. 更换 伪装网站"
-    green " 4. 更换 BBR加速"
+    green "  1. 安装 V2ray+ws+tls"
+    green "  2. 更新 V2ray主程序"
+    green "  3. 更换 伪装网站"
+    green "  4. 更换 BBR加速"
     green " --------------------------------------------------------"
-    green " 5. 重启 V2ray+Nginx"
-    green " 6. 停止 V2ray+Nginx"
-    green " 7. 卸载 V2ray+Nginx"
-    green " 8. 退出"
+    green "  5. 编辑 V2ray配置"
+    green "  6. 编辑 Nginx配置"
+    green "  7. 查看 账号信息"
+    green " --------------------------------------------------------"
+    green "  8. 重启 V2ray+Nginx"
+    green "  9. 停止 V2ray+Nginx"
+    green " 10. 卸载 V2ray+Nginx"
+    green " 11. 退出脚本"
     echo
     read -p "请输入一个数字后回车:" num
     case "$num" in
@@ -583,6 +604,7 @@ function start_menu(){
     check_env
     install
     install_bbr
+    start_menu
     ;;
     2)
     update_v2ray
@@ -597,30 +619,45 @@ function start_menu(){
     start_menu
     ;;
     5)
-    systemctl restart v2ray.service >/dev/null 2>&1
-    systemctl restart nginx.service >/dev/null 2>&1
-    green "服务重启完成！"
-    sleep 6
+    vim /usr/local/etc/v2ray/config.json
+    green "5秒后返回"
+    sleep 5
     start_menu
     ;;
     6)
-    systemctl stop v2ray.service >/dev/null 2>&1
-    systemctl stop nginx.service >/dev/null 2>&1
-    green "服务停止完成！"
-    sleep 6
+    vim /etc/nginx/conf/nginx.conf
+    green "5秒后返回"
+    sleep 5
     start_menu
     ;;
     7)
+    cat /usr/local/etc/v2ray/myconfig.json
+    ;;
+    8)
+    systemctl restart v2ray.service >/dev/null 2>&1
+    systemctl restart nginx.service >/dev/null 2>&1
+    green "v2ray+nginx服务已重启！"
+    sleep 5
+    start_menu
+    ;;
+    9)
+    systemctl stop v2ray.service >/dev/null 2>&1
+    systemctl stop nginx.service >/dev/null 2>&1
+    green "v2ray+nginx服务已停止！"
+    sleep 5
+    start_menu
+    ;;
+    10)
     remove_v2ray_nginx
     start_menu
     ;;
-    8)
+    11)
     exit 1
     ;;
     *)
     clear
     red "请输入正确的数字！"
-    sleep 6
+    sleep 5
     start_menu
     ;;
     esac
